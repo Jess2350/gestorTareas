84b4df1d41697812c990e83677a2307be2772aa2
Jess2350 <you@example.com>
2024-08-22 03:51:10 -0600

    Función agregar tarea, actualizar tarea, eliminar tarea, mostrar tareas a usuarios e inicio de index.JS

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+import "./styles.css";
+import { addTask, deleteTsk, toggleTask } from "./task";
+import { renderTasks } from "./ui";
 
+document.addEventListener("DOMContentLoaded", () => {
+    renderTasks();
+
+    document.getElementsById("task-form").addEventListener("submit", (e) => {
+        e.preventDefault();
+
+        const taskInput = document.getElementsById("task-input").value;
+    });
+});
--- src/task.js
+++ src/task.js
@@ -1,1 +1,33 @@
+// Traemos la lista de tareas del almacenamiento local
 let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+// Funcion para agregar una tarea
+export const addTask = (task) => {
+    const newTask = {
+        id: Date.now(),
+        text: task,
+        completed: false,
+    };
+    task.push(newTask);
+    localStorage.setItem("tasks", JSON.stringify(task));
+};
+
+// Funcion para eliminar una tarea
+export const deleteTask = (id) => {
+    tasks = tasks.filter((task) => task.id !== parseInt(id));
+    localStorage.setItem("tasks", JSON.stringify(task));
+};
+
+// Funcion para actualizar la tarea
+export const toggleTask = (id) => {
+    tasks = tasks.map((task) => {
+        if(task.id === parseInt(id)) {
+            task.completed = !task.completed;
+        }
+        return task;
+    });
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+};
+
+// Funcion para llevar las tareas
+export const getTasks = () => tasks;
