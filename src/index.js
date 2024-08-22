fa4fcf980cbfa498862bd1225a9131d61782b921
Jess2350 <you@example.com>
2024-08-22 04:47:43 -0600

    Actualizazcion

--- src/index.js
+++ src/index.js
@@ -9,5 +9,25 @@ document.addEventListener("DOMContentLoaded", () => {
         e.preventDefault();
 
         const taskInput = document.getElementsById("task-input").value;
+        if(taskInput !== "") {
+            addTask(taskInput);
+            renderTasks();
+            // Limpiar el cuadro de texto
+            document.getElementById("task-input").value = "";
+        }
+    });
+
+    document.getElementById("task-list").addEventListener("click", (e) => {
+        if(e.target.classList.contains("delete")) {
+            const taskId = e.target.parentElement.getAttribute("data-id");
+            deleteTask(taskId);
+            renderTasks();
+        }
+
+        if(e.target.classList.contains("toggle")) {
+            const taskId = e.target.parentElement.getAttribute("data-id");
+            toggleTask(taskId);
+            renderTasks();
+        }
     });
 });
